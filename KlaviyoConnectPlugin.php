7468ba5981361d7c177b9fd3089135bbc6f69e6c
Tyrone <tyrone@continuon.co>
2018-01-08 22:54:06 +0200

    Init

---
+++ KlaviyoConnectPlugin.php
@@ -0,0 +1,58 @@
+<?php
+namespace Craft;
+
+class KlaviyoConnectPlugin extends BasePlugin
+{
+    function getName()
+    {
+         return Craft::t('Klaviyo subscription management CraftCMS plugin');
+    }
+
+    function getVersion()
+    {
+        return '1.0';
+    }
+
+    function getDeveloper()
+    {
+        return 'Shoe Shine Design';
+    }
+
+    function getDeveloperUrl()
+    {
+        return 'http://shoeshinedesign.com/';
+    }
+
+    function init()
+    {
+        require_once __DIR__ . '/vendor/autoload.php';
+
+        craft()->on('commerce_orders.onSaveOrder', function($event) {
+            craft()->klaviyoConnect->onSaveOrder($event);
+        });
+
+        craft()->on('commerce_orders.onOrderComplete', function($event) {
+            craft()->klaviyoConnect->onOrderComplete($event);
+        });
+
+        craft()->on('users.onSaveUser', function($event) {
+            craft()->klaviyoConnect->onSaveUser($event);
+        });
+    }
+
+    protected function defineSettings()
+    {
+        return array(
+          'klaviyoSiteId' => array(AttributeType::String, 'default' => ''),
+          'klaviyoApiKey' => array(AttributeType::String, 'default' => ''),
+          'subscribeFieldHandle' => array(AttributeType::String, 'default' => 'newsletter'),
+        );
+    }
+
+    public function getSettingsHtml()
+    {
+        return craft()->templates->render('klaviyoconnect/settings', array(
+          'settings' => $this->getSettings()
+        ));
+    }
+}
---
+++ services/KlaviyoConnectService.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Craft;
+
+use \Klaviyo;
+
+class KlaviyoConnectService extends BaseApplicationComponent
+{
+    var $settings = null;
+
+    function __construct()
+    {
+        $plugin = craft()->plugins->getPlugin('klaviyoconnect');
+        $this->settings = $plugin->getSettings();
+    }
+
+    // Add guests to mailing list
+    public function onSaveOrder($event)
+    {
+        $order = $event->params['order'];
+        $customer = $order->customer;
+
+        $customerProperties = ['$email' => $order->email];
+
+        // Use shipping/billing details to get extra customer info if they're already set
+        if (is_null($customer->user)) {
+            $subscriptionHandle = $this->getSetting('subscribeFieldHandle');
+            if (isset($order[$subscriptionHandle])) {
+                $withSubscription = $order[$subscriptionHandle];
+                $customerProperties['WithSubscription'] = $withSubscription ? 'yes' : 'no';
+            }
+
+            $address = null;
+            if (!is_null($order->shippingAddress)) {
+                $address = $order->shippingAddress;
+            } else if (!is_null($order->billingAddress)) {
+                $address = $order->billingAddress;
+            }
+
+            if (!is_null($address)) {
+                $customerProperties = array_merge($customerProperties, [
+                    '$first_name' => $address->firstName,
+                    '$last_name' => $address->lastName,
+                    '$phone_number' => $address->phone,
+                ]);
+            }
+        } else { // Set details from UserModel
+            $user = $customer->user;
+            $customerProperties = array_merge($customerProperties, [
+                '$email' => $user->email,
+                '$first_name' => $user->firstName,
+                '$last_name' => $user->lastName,
+                'IsRegistered' => 'yes',
+            ]);
+        }
+
+        $lineItemsProperties = array();
+
+        foreach ($order->lineItems as $lineItem) {
+            $product = $lineItem->purchasable->product;
+
+            $lineItemProperties = [
+                'Title' => $product->title,
+                'URL' => '/'.$product->uri,
+                'Price' => $lineItem->price,
+                'Line Price' => $lineItem->subtotal,
+                'Quantity' => $lineItem->qty,
+            ];
+
+            if (isset($product->productImages)) {
+                $images = $product->productImages->find();
+
+                if (sizeof($images) > 0) {
+                    $image = $images[0];
+                    $lineItemProperties['Image'] = $image->getUrl('productThumbnail');
+                }
+            }
+
+            $lineItemsProperties[] = $lineItemProperties;
+        }
+
+        $extraProperties = [
+            'Order ID' => $order->id,
+            'Item Total' => $order->itemTotal,
+            'Total Price' => $order->totalPrice,
+            'Item Count' => $order->totalQty,
+            'Line Items' => $lineItemsProperties,
+        ];
+
+        $klaviyo = new Klaviyo($this->getSetting('klaviyoSiteId'));
+        $klaviyo->track('Cart Updated', $customerProperties, $extraProperties);
+    }
+
+    public function onOrderComplete($event)
+    {
+        $order = $event->params['order'];
+        $klaviyo = new Klaviyo($this->getSetting('klaviyoSiteId'));
+        $klaviyo->track(
+            'Order Complete',
+            ['$email' => $order->email],
+            ['Order ID' => $order->id]
+        );
+    }
+
+    public function onSaveUser($event)
+    {
+        $user = $event->params['user'];
+        $isCustomer = $user->isInGroup('customers');
+
+        if ($isCustomer) {
+            $isNewUser = $event->params['isNewUser'];
+
+            $klaviyo = new Klaviyo($this->getSetting('klaviyoSiteId'));
+            $klaviyo->identify([
+                '$email' => $user->email,
+                '$first_name' => $user->firstName,
+                '$last_name' => $user->lastName,
+                'IsRegistered' => 'yes',
+                'WasUpdated' => ($isNewUser ? 'no' : 'yes'),
+            ]);
+        }
+    }
+
+    public function getSetting($name)
+    {
+        return $this->settings[$name];
+    }
+}
