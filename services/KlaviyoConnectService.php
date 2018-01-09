9c8c2acc3f6ff031c797dcb5c2bcdafd1cdeb955
Tyrone <tyrone@continuon.co>
2018-01-09 23:22:11 +0200

    Check if an email address is set on an order; Fixes #1

--- services/KlaviyoConnectService.php
+++ services/KlaviyoConnectService.php
@@ -18,77 +18,88 @@ class KlaviyoConnectService extends BaseApplicationComponent
     public function onSaveOrder($event)
     {
         $order = $event->params['order'];
-        $customer = $order->customer;
 
-        $customerProperties = ['$email' => $order->email];
+        if (!$order->isCompleted) {
+            $customer = $order->customer;
 
-        // Use shipping/billing details to get extra customer info if they're already set
-        if (is_null($customer->user)) {
-            $subscriptionHandle = $this->getSetting('subscribeFieldHandle');
-            if (isset($order[$subscriptionHandle])) {
-                $withSubscription = $order[$subscriptionHandle];
-                $customerProperties['WithSubscription'] = $withSubscription ? 'yes' : 'no';
-            }
-
-            $address = null;
-            if (!is_null($order->shippingAddress)) {
-                $address = $order->shippingAddress;
-            } else if (!is_null($order->billingAddress)) {
-                $address = $order->billingAddress;
-            }
-
-            if (!is_null($address)) {
-                $customerProperties = array_merge($customerProperties, [
-                    '$first_name' => $address->firstName,
-                    '$last_name' => $address->lastName,
-                    '$phone_number' => $address->phone,
-                ]);
-            }
-        } else { // Set details from UserModel
-            $user = $customer->user;
-            $customerProperties = array_merge($customerProperties, [
-                '$email' => $user->email,
-                '$first_name' => $user->firstName,
-                '$last_name' => $user->lastName,
-                'IsRegistered' => 'yes',
-            ]);
-        }
-
-        $lineItemsProperties = array();
-
-        foreach ($order->lineItems as $lineItem) {
-            $product = $lineItem->purchasable->product;
-
-            $lineItemProperties = [
-                'Title' => $product->title,
-                'URL' => '/'.$product->uri,
-                'Price' => $lineItem->price,
-                'Line Price' => $lineItem->subtotal,
-                'Quantity' => $lineItem->qty,
-            ];
-
-            if (isset($product->productImages)) {
-                $images = $product->productImages->find();
-
-                if (sizeof($images) > 0) {
-                    $image = $images[0];
-                    $lineItemProperties['Image'] = $image->getUrl('productThumbnail');
+            $email = $order->email;
+            if (is_null($email)) {
+                if (!is_null($customer->user)) {
+                    $email = $customer->user->email;
                 }
             }
 
-            $lineItemsProperties[] = $lineItemProperties;
+            if (!is_null($email)) {
+                $customerProperties = ['$email' => $email];
+
+                // Use shipping/billing details to get extra customer info if they're already set
+                if (is_null($customer->user)) {
+                    $subscriptionHandle = $this->getSetting('subscribeFieldHandle');
+                    if (isset($order[$subscriptionHandle])) {
+                        $withSubscription = $order[$subscriptionHandle];
+                        $customerProperties['WithSubscription'] = $withSubscription ? 'yes' : 'no';
+                    }
+
+                    $address = null;
+                    if (!is_null($order->shippingAddress)) {
+                        $address = $order->shippingAddress;
+                    } else if (!is_null($order->billingAddress)) {
+                        $address = $order->billingAddress;
+                    }
+
+                    if (!is_null($address)) {
+                        $customerProperties = array_merge($customerProperties, [
+                            '$first_name' => $address->firstName,
+                            '$last_name' => $address->lastName,
+                            '$phone_number' => $address->phone,
+                        ]);
+                    }
+                } else { // Set details from UserModel
+                    $user = $customer->user;
+                    $customerProperties = array_merge($customerProperties, [
+                        '$first_name' => $user->firstName,
+                        '$last_name' => $user->lastName,
+                        'IsRegistered' => 'yes',
+                    ]);
+                }
+
+                $lineItemsProperties = array();
+
+                foreach ($order->lineItems as $lineItem) {
+                    $product = $lineItem->purchasable->product;
+
+                    $lineItemProperties = [
+                        'Title' => $product->title,
+                        'URL' => '/'.$product->uri,
+                        'Price' => $lineItem->price,
+                        'Line Price' => $lineItem->subtotal,
+                        'Quantity' => $lineItem->qty,
+                    ];
+
+                    if (isset($product->productImages)) {
+                        $images = $product->productImages->find();
+
+                        if (sizeof($images) > 0) {
+                            $image = $images[0];
+                            $lineItemProperties['Image'] = $image->getUrl('productThumbnail');
+                        }
+                    }
+
+                    $lineItemsProperties[] = $lineItemProperties;
+                }
+
+                $extraProperties = [
+                    'Order ID' => $order->id,
+                    'Item Total' => $order->itemTotal,
+                    'Total Price' => $order->totalPrice,
+                    'Item Count' => $order->totalQty,
+                    'Line Items' => $lineItemsProperties,
+                ];
+
+                $klaviyo = new Klaviyo($this->getSetting('klaviyoSiteId'));
+                $klaviyo->track('Cart Updated', $customerProperties, $extraProperties);
+            }
         }
-
-        $extraProperties = [
-            'Order ID' => $order->id,
-            'Item Total' => $order->itemTotal,
-            'Total Price' => $order->totalPrice,
-            'Item Count' => $order->totalQty,
-            'Line Items' => $lineItemsProperties,
-        ];
-
-        $klaviyo = new Klaviyo($this->getSetting('klaviyoSiteId'));
-        $klaviyo->track('Cart Updated', $customerProperties, $extraProperties);
     }
 
     public function onOrderComplete($event)
