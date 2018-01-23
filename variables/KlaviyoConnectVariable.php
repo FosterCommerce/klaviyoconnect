accbd085def284cd343822de8ff732c8c3b80e67
Tyrone <tyrone@continuon.co>
2018-01-23 15:05:53 +0200

    Add try...catch for plugin settings when retrieve lists from Klaviyo

--- variables/KlaviyoConnectVariable.php
+++ variables/KlaviyoConnectVariable.php
@@ -1,16 +1,32 @@
 <?php
 namespace Craft;
 
+use \GuzzleHttp\Exception\RequestException;
+
 class KlaviyoConnectVariable
 {
+    private $error = null;
     private $lists = null;
 
     public function lists()
     {
         if (is_null($this->lists)) {
-            $this->lists = craft()->klaviyoConnect_api->getLists();
+            try {
+                $lists = craft()->klaviyoConnect_api->getLists();
+                if (sizeof($lists) > 0) {
+                    $this->lists = $lists;
+                }
+            } catch (RequestException $e) {
+                $response = json_decode($e->getResponse()->getBody()->getContents());
+                $this->error = [ $response->status => $response->message];
+            }
         }
         return $this->lists;
+    }
+
+    public function error()
+    {
+        return $this->error;
     }
 
     public function profileMappingProviders()
