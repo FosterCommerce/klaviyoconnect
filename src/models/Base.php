a95159efa3d4ce1b3e48fb72b0002c37b216ff5c
Tyrone Tudehope <tyronetudehope@gmail.com>
2019-05-20 15:12:59 +0200

    Add consent properties to the Profile model

--- src/models/Base.php
+++ src/models/Base.php
@@ -19,16 +19,6 @@ abstract class Base extends Model
         }
     }
 
-    public function getSpecialProperties(): Array
-    {
-        $specialProps = (new \ReflectionClass($this))->getProperties();
-        $specialProps = array_map(function ($item) {
-            return $item->name;
-        }, $specialProps);
-
-        return $specialProps;
-    }
-
     public function setCustomProperties($properties)
     {
         foreach ($properties as $property => $value) {
@@ -40,8 +30,6 @@ abstract class Base extends Model
     {
         $arr = parent::toArray($fields, $expand, $recursive);
 
-        $specialProps = $this->attributes();
-
         $mapped = [];
         foreach ($arr as $name => $value) {
             $mapped["\${$name}"] = $value;
@@ -50,17 +38,6 @@ abstract class Base extends Model
         foreach ($this->customAttributes as $name) {
             $mapped[$name] = $this->$name;
         }
-
-        // $mapped = [];
-        // foreach ($arr as $key => $value) {
-        //     if ($value) {
-        //         if (in_array($key, $specialProps)) {
-        //             $mapped["\${$key}"] = $value;
-        //         } else {
-        //             $mapped[$key] = $value;
-        //         }
-        //     }
-        // }
 
         return $mapped;
     }
--- src/models/Profile.php
+++ src/models/Profile.php
@@ -19,6 +19,24 @@ class Profile extends Base
     public $zip;
     public $image;
 
+    /**
+     * GDPR related properties
+     * See: https://help.klaviyo.com/hc/en-us/articles/360003536031-Collect-GDPR-Compliant-Consent
+     */
+
+    /**
+     * One of "email", "web", "sms", "directmail", "mobile"
+     */
+    public $consent;
+    public $consent_id;
+    public $consent_method;
+
+    /**
+     * Automatically added by Klaviyo if not present
+     */
+    public $consent_timestamp;
+    public $consent_version;
+
     public function __toString()
     {
         return $this->id ? $this->id : $this->email;
