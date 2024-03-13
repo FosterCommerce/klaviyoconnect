e820c87ecd7a1ebcc564e141e3181c1c10ea8622
Tyrone Tudehope <tyronetudehope@gmail.com>
2024-03-13 13:25:33 +0200

    chore: Configure code quality tools

--- ecs.php
+++ ecs.php
@@ -2,15 +2,16 @@
 
 declare(strict_types=1);
 
-use craft\ecs\SetList;
+use fostercommerce\ecs\SetList;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function(ECSConfig $ecsConfig): void {
-    $ecsConfig->parallel();
     $ecsConfig->paths([
         __DIR__ . '/src',
         __FILE__,
     ]);
 
-    $ecsConfig->sets([SetList::CRAFT_CMS_4]);
+    $ecsConfig->sets([
+        SetList::CRAFT_CMS_4,
+    ]);
 };
--- rector.php
+++ rector.php
@@ -1,8 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-use craft\rector\SetList as CraftSetList;
-use Rector\Set\ValueObject\SetList;
+use fostercommerce\rector\SetList;
 use Rector\Config\RectorConfig;
 
 return static function(RectorConfig $rectorConfig): void {
@@ -11,27 +10,6 @@ return static function(RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->sets([
-        SetList::PHP_81,
-        SetList::PHP_80,
-        SetList::PHP_74,
-        SetList::PHP_73,
-        SetList::PHP_72,
-        SetList::PHP_71,
-        SetList::PHP_70,
-        SetList::PHP_56,
-        SetList::PHP_55,
-        SetList::PHP_54,
-        SetList::PHP_53,
-        SetList::PHP_52,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::DEAD_CODE,
-        SetList::STRICT_BOOLEANS,
-        SetList::NAMING,
-        SetList::TYPE_DECLARATION,
-        SetList::EARLY_RETURN,
-        SetList::INSTANCEOF,
-        CraftSetList::CRAFT_CMS_40,
-        CraftSetList::CRAFT_COMMERCE_40,
+        SetList::CRAFT_CMS_40
     ]);
 };
